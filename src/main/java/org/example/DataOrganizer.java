de02d969fcc91ff49c86ed81854a1533fdd9d741
Yehia <58986971+yehiasamehsaleh@users.noreply.github.com>
2025-01-24 00:01:49 +0200

    Update DataOrganizer.java

--- src/main/java/org/example/DataOrganizer.java
+++ src/main/java/org/example/DataOrganizer.java
@@ -124,7 +124,7 @@ public class DataOrganizer {
         return sortedData;
     }
 
-    private static int CompareObjects(Object o1, Object o2) {
+    private static int CompareObjects(Object o1, Object o2) {  // default Timsort
         if (o1 == null && o2 == null) return 0;
         if (o1 == null) return 1;
         if (o2 == null) return -1;
